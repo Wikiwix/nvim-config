3b4f1fbb1af91d512b7ace7ceb46c879540349b6
Philip Wintersteiner <philip.wintersteiner@gmx.de>
2025-09-09 17:55:22 +0200

    Add router

--- init.lua
+++ init.lua
@@ -842,6 +842,12 @@ require('lazy').setup({
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
   {
+    'notjedi/nvim-rooter.lua',
+    config = function()
+      require('nvim-rooter').setup()
+    end,
+  },
+  {
     'edte/normal-colon.nvim',
     opts = {},
   },
