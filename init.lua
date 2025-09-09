138abf2146aa2440d9bdd7d825e883ee014890fb
Philip Wintersteiner <philip.wintersteiner@gmx.de>
2025-09-09 10:19:01 +0200

    Add bar and normalcolon

--- init.lua
+++ init.lua
@@ -829,6 +829,22 @@ require('lazy').setup({
       require('nvim-surround').setup {}
     end,
   },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {},
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    'edte/normal-colon.nvim',
+    opts = {},
+  },
   { -- Collection of various small independent plugins/modules
     'nvim-mini/mini.nvim',
     config = function()
